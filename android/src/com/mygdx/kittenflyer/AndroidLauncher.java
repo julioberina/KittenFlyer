45da83c183f9a540049a9f1967887789ad392288
Julio Berina <julioberina@gmail.com>
2016-12-18 00:51:57 -0800

    First commit

---
+++ android/src/com/mygdx/kittenflyer/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.kittenflyer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.kittenflyer.KittenFlyer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KittenFlyer(), config);
+	}
+}
